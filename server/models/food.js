68718400bd40eee42c07ec76ea69b58bcb2a9b51
Yong Kim <yongk.adds@gmail.com>
2016-05-15 19:51:15 -0400

    Final version

--- server/models/food.js
+++ server/models/food.js
@@ -1,3 +1,34 @@
 module.exports = function(Food) {
 
+  var app = require('../../server/server');
+  var async = require('async');
+  var Fridge;
+
+  // REMOTE FUNCTIONS
+  Food.GetFridgesByFood = function(foodName, cb) {
+
+    var Fridge = app.models.Fridge;
+    var fridges = [];
+    Food.find({where: {name:foodName}})
+      .then(function(foods) {
+
+        async.eachSeries(foods, function(food, callback) {
+          Fridge.find({where: {id: food.fridgeId}})
+            .then(function(fridge) {
+              fridges.push(fridge[0]);
+              callback();
+            })
+        }, function(err) {
+          cb(null, fridges);
+        })
+      }).catch(function(err) {cb(err, null);});
+
+  }
+
+  Food.remoteMethod('GetFridgesByFood', {
+    http: {path: '/getFridgesByFood', verb: 'get'},
+    accepts: {args: 'foodName', type: 'string'},
+    returns: {type: 'object', root:true}
+  })
+
 };
